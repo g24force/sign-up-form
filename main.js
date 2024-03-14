0e7214db9c617b64270d364c89a37ca933409ed6
g24force <102439759+g24force@users.noreply.github.com>
2024-03-14 16:41:42 +0000

    Check passwords when input value changes

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
 const passwordMismatchError = document.querySelector('p.password-mismatch-error')
 
-password.addEventListener("blur", () => {
+password.addEventListener("input", () => {
     comparePasswordFields ();
 });
 
-confirmPassword.addEventListener("blur", () => {
+confirmPassword.addEventListener("input", () => {
     comparePasswordFields ();
 });
 
 function comparePasswordFields () {
-    if (password.value !== undefined && confirmPassword.value !== undefined) {
+    if ((password.value !== undefined || password.value !== "") && (confirmPassword.value !== undefined|| confirmPassword.value !== "")) {
         if (password.value === confirmPassword.value) {
             password.classList.remove("error");
             confirmPassword.classList.remove("error");
             passwordMismatchError.textContent = '';
+            document.querySelector("#submit-button").disabled = false;
         }
     }
 };
