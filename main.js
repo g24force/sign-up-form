e91459460ff02e1deac2043d0cfcac7bba5beedd
g24force <102439759+g24force@users.noreply.github.com>
2024-03-14 16:50:00 +0000

    Make sure error is shown if passwords don't match

--- main.js
+++ main.js
@@ -12,7 +12,14 @@ confirmPassword.addEventListener("input", () => {
 
 function comparePasswordFields () {
     if ((password.value !== undefined || password.value !== "") && (confirmPassword.value !== undefined|| confirmPassword.value !== "")) {
-        if (password.value === confirmPassword.value) {
+        if (password.value !== confirmPassword.value) {
+            passwordMismatchError.textContent = '* Passwords do not match';
+            if (password.classList.contains('error') !== true ) {
+                password.classList.add("error");
+                confirmPassword.classList.add("error");
+                document.querySelector("#submit-button").disabled = true;
+            }
+        } else {
             password.classList.remove("error");
             confirmPassword.classList.remove("error");
             passwordMismatchError.textContent = '';
